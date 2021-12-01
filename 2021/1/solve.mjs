9fb90a2489b5ad69cfe078971d012842879cabe2
mrrogge <5900418+mrrogge@users.noreply.github.com>
2021-11-30 23:25:49 -0600

    2021 day 1

--- 2021/1/solve.mjs
+++ 2021/1/solve.mjs
@@ -1,3 +1,31 @@
 import AOCTools from "../tools/AOCTools.mjs";
 
-let input = AOCTools.parseInput();
+let input = await AOCTools.parseInput();
+
+input = input.map((v) => Number.parseInt(v));
+let prev = null;
+let i = 0;
+input.forEach(v => {
+    if (prev !== null && v > prev) {
+        i++;
+    }
+    prev = v;
+});
+console.log(i);
+
+let a = null;
+let b = null;
+let c = null;
+let prevSum = null;
+let j = 0;
+input.forEach(v => {
+    if (a !== null && b !== null && c !== null) {
+        let sum = a+b+c;
+        if (prevSum !== null && sum > prevSum) j++;
+        prevSum = sum;
+    }
+    c = b;
+    b = a;
+    a = v;
+})
+console.log(j);
