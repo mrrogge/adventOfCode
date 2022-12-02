bcd889b286b27e50682df3e0fcdc7cd688f4ed1c
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-12-01 18:08:57 -0600

    y2015 d8 p2

--- sessions/20221127/y2015/d8/main.scala
+++ sessions/20221127/y2015/d8/main.scala
@@ -14,7 +14,25 @@ import scala.io.Source
         }
     }
     val lengths2 = v1.map(x => evalLine(0, x.toList))
-    println(lengths1)
-    println(lengths2)
+
     println(lengths1.zip(lengths2).map(x => x._1-x._2).fold(0)((a,b)=>a+b))
+
+    def shortToLongString(short:List[Char]):List[Char] = {
+        ("\"" ++ shortToLongStringInner(List.empty, short) ++ "\"").toList
+    }
+
+    def shortToLongStringInner(acc:List[Char], line:List[Char]):List[Char] = {
+        line match {
+            case '"' :: xs => shortToLongStringInner(acc ++ "\\\"".toList, xs)
+            case '\\' :: xs => shortToLongStringInner(acc ++ "\\\\".toList, xs)
+            case x :: xs => shortToLongStringInner(acc :+ x, xs)
+            case Nil => acc
+        }
+    }
+
+    val v2 = v1.map(_.toList).map(shortToLongString).map(_.length)
+    val v3 = v2.zip(lengths1)
+    .map(x => x._1-x._2)
+    .fold(0)(_+_)
+    println(v3)
 }
