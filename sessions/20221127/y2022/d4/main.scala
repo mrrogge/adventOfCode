4d6a2d519c66f18e85857ed9fcd36c3b29c79b4e
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-12-03 23:21:43 -0600

    y2022 d4 p2 as solved

--- sessions/20221127/y2022/d4/main.scala
+++ sessions/20221127/y2022/d4/main.scala
@@ -11,7 +11,7 @@ import scala.io.Source
         case _ => throw new Exception
     })
     .map(a => (Set.range(a._1._1, a._1._2+1), Set.range(a._2._1, a._2._2+1)))
-    .map(a => a._1.subsetOf(a._2) | a._2.subsetOf(a._1))
+    .map(a => !a._1.intersect(a._2).isEmpty)
     .count(a => a)
     println(v1)
 
