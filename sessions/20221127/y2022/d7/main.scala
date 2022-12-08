89a94366732c2a5e99f992f8bda4746523053c3c
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-12-08 08:14:21 -0600

    .

--- sessions/20221127/y2022/d7/main.scala
+++ sessions/20221127/y2022/d7/main.scala
@@ -1,47 +1,50 @@
 import scala.io.Source
-import scala.annotation.tailrec
 
-// parents, children, sizes, currentPath, lastCmd
-type FS = (Map[String, String], Map[String, Set[String]], Map[String, Int], String, String)
-
+// sizes, currentPath, lastCmd
+type FS = (Map[String, (String, Int)], String, String)
 
 @main def main = {
-    val moves = Source.fromFile("test").getLines().toList
-    .map(_.split(" ").toList)
+    val moves = Source.fromFile("input").getLines().toList
+    .map(_.split(" ").toList).drop(1)
 
-    var fs:FS = (Map("/" -> "#"), Map("/" -> Set.empty), Map("/" -> 0), "/", "")
+    var fs:FS = (Map("/" -> ("dir", 0)), "/", "cd")
 
     fs = moves.foldLeft(fs)((fs, move) => {
         move match {
+            case "$" :: "cd" :: ".." :: Nil => {
+                (fs._1, fs._2.split("/").toList.dropRight(1).mkString("/")+"/", "cd")
+            }
             case "$" :: "cd" :: path :: Nil => {
-                (fs._1, fs._2, fs._3, path, "cd")
+                (fs._1, s"${fs._2}${path}/", "cd")
             }
             case "$" :: "ls" :: Nil => {
-                (fs._1, fs._2, fs._3, fs._4, "ls")
+                (fs._1, fs._2, "ls")
             }
-            case x1 :: x2 :: Nil if fs._5 == "ls" => {
+            case x1 :: x2 :: Nil if fs._3 == "ls" => {
                 if x1 == "dir" then {
-                    (fs._1+(x2 -> fs._4), fs._2+(fs._4 -> (fs._2.applyOrElse(fs._4, _ => Set.empty)+x2)), fs._3+(x2 -> 0), fs._4, fs._5)
+                    (fs._1+(s"${fs._2}${x2}/" -> ("dir", 0)), fs._2, fs._3)
                 }
                 else {
-                    (fs._1+(x2 -> fs._4), fs._2+(fs._4 -> (fs._2.applyOrElse(fs._4, _ => Set.empty)+x2)), fs._3+(x2 -> x1.toInt), fs._4, fs._5)
+                    val filePath = s"${fs._2}${x2}"
+                    val parentSections = fs._2.split("/").toList
+                    val parentList = parentSections.foldLeft((List[String](), parentSections))((acc, _) => {
+                        val parentPath = acc._2.mkString("/")+"/"
+                        (parentPath :: acc._1, acc._2.dropRight(1))
+                    })._1
+                    val sizes = (fs._1+(filePath -> ("file", x1.toInt)))
+                        .map((path,spec) => {
+                            (path, (spec._1, if parentList.contains(path) then spec._2+x1.toInt else spec._2))
+                        })
+                    (sizes, fs._2, fs._3)
+                    
+
+                    // (fs._1+(x2 -> fs._4), fs._2+(fs._4 -> (fs._2.applyOrElse(fs._4, _ => Set.empty)+x2)), fs._3+(x2 -> x1.toInt), fs._4, fs._5)
                 }
             }
             case _ => throw new Exception
         }
     })
+    println(fs._1)
+    println(fs._1.values.filter(x => x._1 == "dir").map(_._2).filter(_<=100000).sum)
 
-    def getDirSize(fs:FS, path:String):Int = {
-        if !fs._2.contains(path) then {
-            fs._3(path)
-        }
-        else {
-            fs._2(path).toList.map(x => getDirSize(fs, x)).sum
-        }
-    }
-
-    fs = (fs._1, fs._2, fs._3.map((k,_) => (k, getDirSize(fs, k))), fs._4, fs._5)
-
-    val v1 = fs._2.map((k,_)=>fs._3(k)).filter(_<=100000).sum
-    println(v1)
 }
