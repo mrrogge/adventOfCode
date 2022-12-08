780681070a4790f9124133348f3910245437f62c
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-12-08 09:46:10 -0600

    Not sure, think I was trying to clean it up, oh well

--- sessions/20221127/y2022/d5/main.scala
+++ sessions/20221127/y2022/d5/main.scala
@@ -1,5 +1,6 @@
 import scala.io.Source
 import scala.collection.mutable.ArrayBuffer
+import scala.util.Using
 
 def stackLineToBlocks(acc:List[Option[Char]], rem:List[Char]):List[Option[Char]] = {
     rem match {
@@ -11,8 +12,9 @@ def stackLineToBlocks(acc:List[Option[Char]], rem:List[Char]):List[Option[Char]]
     }
 }
 
-@main def main = {
+def main = {
     val lines = Source.fromFile("input").getLines().toList
+    val lines2 = Source.fromFile("input")("utf-8")
     val stackLines = lines.takeWhile(_ != "")
     val moveLines = lines.dropWhile(_ != "").tail
 
@@ -53,4 +55,26 @@ def stackLineToBlocks(acc:List[Option[Char]], rem:List[Char]):List[Option[Char]]
 
     println(stacks.map(x => x.head))
 
+}
+
+@main def main2 = {
+    val (stacks, moves) = (Source.fromFile("input")
+    .mkString.split("\n\n")
+    .map(_.split("\n"))
+    .toList match {
+        case x1 :: x2 :: Nil => (
+            x1.take(x1.length-1).reverse
+            .map(x => x.grouped(4).toArray)
+            , 
+            x2.map(_.split(" ").toList)
+            .map(_ match {
+                case "move" :: x1 :: "from" :: x2 :: "to" :: x3 :: xs => (x1.toInt, x2.toInt-1, x3.toInt-1)
+                case _ => throw new Exception
+            })
+        )
+        case _ => throw new Exception
+    })
+    println(stacks)
+    
+    
 }
