9f0342d272ee2d7fbb0afa1bec3922b213f620ea
mrrogge <5900418+mrrogge@users.noreply.github.com>
2022-12-13 22:09:15 -0600

    .

--- sessions/20221127/y2022/d9/main.scala
+++ sessions/20221127/y2022/d9/main.scala
@@ -1,7 +1,7 @@
 import scala.io.Source
 
 @main def main = {
-    val moves = Source.fromFile("input").getLines().map(_.split(" "))
+    val moves = Source.fromFile("test2").getLines().map(_.split(" "))
     .map(_.toList match {
         case x1 :: x2 :: Nil => (x1, x2.toInt)
         case _ => throw new Exception
@@ -42,4 +42,45 @@ import scala.io.Source
     val v2 = v1.cells.map((y, row) => row.count((x, visited) => visited)).sum
 
     println(v2)
+
+    case class State2 (
+        visited: Set[(Int, Int)],
+        nodes: Vector[(Int, Int)],
+        prevNodes: Vector[(Int, Int)]
+    )
+
+    val initState2 = State2(Set((0,0)), Vector.fill(9)((0,0)), Vector.fill(9)((0,0)))
+
+    def step2(state:State2, dir:String):State2 = {
+        var prevNodes = state.prevNodes.updated(0, state.nodes(0))
+        var visited = state.visited
+        val prevHead = prevNodes(0)
+        val newHead = dir match {
+            case "L" => (prevHead._1-1, prevHead._2)
+            case "R" => (prevHead._1+1, prevHead._2)
+            case "U" => (prevHead._1, prevHead._2-1)
+            case "D" => (prevHead._1, prevHead._2+1)
+        }
+        var nodes = state.nodes.updated(0, newHead)
+        (1 to 8).foreach(i => {
+            prevNodes = prevNodes.updated(i, nodes(i))
+            nodes.updated(i, follow(nodes(i), prevNodes(i-1), nodes(i-1)))
+        })
+        visited = visited + nodes(8)
+        State2(state.visited, nodes, prevNodes)
+    }
+
+    def follow(orig:(Int, Int), prevTarget:(Int, Int), newTarget:(Int, Int)):(Int, Int) = {
+        val diff = (Math.abs(newTarget._1-orig._1), Math.abs(newTarget._2-orig._2))
+        if diff._1 > 1 | diff._2 > 1 then prevTarget else orig
+    }
+
+    val v3 = moves.foldLeft(initState2)((state, move) => {
+        (0 until move._2).foldLeft(state)((state, _) => step2(state, move._1))
+    })
+
+    val v4 = v3.visited.count(_ => true)
+
+    println(v4)
+
 }
